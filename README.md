5932a6af50509f286deb89d593677aa7f1ddd011
DervixDev <fernandobenitezxd22@gmail.com>
2026-02-15 09:52:07 -0600

    Day 2 -if else and scanf

---
+++ README.md
@@ -0,0 +1,14 @@
+# C Learning 🚀
+
+Estoy aprendiendo C desde 0.
+
+## Día 1
+- Hola mundo
+- Variables
+- printf y scanf
+
+## Objetivo
+Aprender programación en C todos los días y subir mis avances.
+
+## Progreso
+Día 1 ✔
---
+++ dia2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+   
+    int numero;
+
+printf("Coloque un numero:");
+
+    scanf("%d", &numero);
+
+    if (numero > 0) {
+        printf("El numero es positivo\n");
+    } else if (numero < 0) {
+        printf("El numero es negativo\n");
+    } else {
+        printf("El numero es cero\n");
+    }
+    return 0;
+
+}
